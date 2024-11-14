56fc62d9fe18f9d86786d56771a6ef678fc21a59
vezaadiloviana <vezaadiloviana27@gmail.com>
2024-11-14 23:52:43 +0700

    instal react router + change to component

---
+++ src/page/Register/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Multistep from '../../components/Multistep'
+
+function Register() {
+  return (
+    <div>
+        <div className="flex justify-center items-center min-w-screen min-h-screen bg-gradient-to-r from-cyan-500 to-blue-500 ">
+            <Multistep/>
+        </div>
+    </div>
+  )
+}
+
+export default Register
